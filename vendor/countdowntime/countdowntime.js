12866c63727f249f68597bc5ca68b493d62898c9
Arman Yazdani <arman.yazdani.10@gmail.com>
2021-06-12 14:09:43 +0430

    First commit

---
+++ vendor/countdowntime/countdowntime.js
@@ -0,0 +1,60 @@
+(function ($) {
+    "use strict";
+
+    $.fn.extend({ 
+
+      countdown100: function(options) {
+        var defaults = {
+          timeZone: "",
+          endtimeYear: 0,
+          endtimeMonth: 0,
+          endtimeDate: 0,
+          endtimeHours: 0,
+          endtimeMinutes: 0,
+          endtimeSeconds: 0,
+        }
+
+        var options =  $.extend(defaults, options);
+
+        return this.each(function() {
+          var obj = $(this);
+          var timeNow = new Date();
+
+          var tZ = options.timeZone;
+          var endYear = options.endtimeYear;
+          var endMonth = options.endtimeMonth;
+          var endDate = options.endtimeDate;
+          var endHours = options.endtimeHours;
+          var endMinutes = options.endtimeMinutes;
+          var endSeconds = options.endtimeSeconds;
+
+          --endYear;
+          do {
+            ++endYear;
+            if(tZ == "") {
+              var deadline = new Date(endYear, endMonth - 1, endDate, endHours, endMinutes, endSeconds);
+            } 
+            else {
+              var deadline = moment.tz([endYear, endMonth - 1, endDate, endHours, endMinutes, endSeconds], tZ).format();
+            }
+          } while(Date.parse(deadline) < Date.parse(timeNow))
+
+          // while(Date.parse(deadline) < Date.parse(timeNow)) {
+          //   var deadline = new Date(Date.parse(new Date()) + endDate * 24 * 60 * 60 * 1000 + endHours * 60 * 60 * 1000); 
+          // }
+          
+          var t = Date.parse(deadline) - Date.parse(new Date());
+            
+          var clock = $(obj).FlipClock(t/1000, {
+            clockFace: 'DailyCounter',
+            countdown: true
+          });
+
+
+        });
+      }
+    });
+
+    
+
+})(jQuery);
